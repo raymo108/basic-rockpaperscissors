330add9de5f4311b704dfc2b0ba0d68478ccc3ab
Shane Raymond <60206015+raymo108@users.noreply.github.com>
2023-07-16 20:32:18 -0400

    Modify game function to increment total points
    Added increment functions to add up and sum point changes between
    user and computer after each round. Included dialogue to declare a
    winner at the end of 5 rounds based on total points.

--- rps.js
+++ rps.js
@@ -7,47 +7,47 @@ function getComputerChoice(options) {      // Create a function called getComput
 
 }
 
+let computerCounter = 0;
+let userCounter = 0;
+
 function playRound (playerSelection, computerSelection) {
     // convert playerSelection to lowercase no matter what for case-sensitivty
     playerSelection = playerSelection.toLowerCase();
     console.log("Computer selected " + computerSelection);
   
-    
-    let computerCounter = 0;
-    let userCounter = 0;
  
     if (playerSelection === 'rock' && computerSelection === 'paper') {
-        computerCounter++;
+        incrementComputerCount();
         console.log("Computer score is " + computerCounter);
         console.log("User Score is " + userCounter)
         return 'You Lose! Paper beats Rock! 1 point for Computer';
     }
         else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            computerCounter++
+            incrementComputerCount()
             console.log("Computer score is " + computerCounter);
             console.log("User Score is " + userCounter)
             return 'You Lose! Scissors beats Paper! 1 point for Computer';
         }
         else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            computerCounter++
+            incrementComputerCount()
             console.log("Computer score is " + computerCounter);
             console.log("User Score is " + userCounter)
             return 'You Lose! Rock beats Scissors! 1 point for Computer';
         }
         else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            userCounter++
+            incrementUsercount()
             console.log("Computer score is " + computerCounter);
             console.log("User Score is " + userCounter)
             return 'You Win! Rock beats Scissors! You receive 1 point';
         }
         else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            userCounter++
+            incrementUsercount()
             console.log("Computer score is " + computerCounter);
             console.log("User Score is " + userCounter)
             return 'You Win! Paper beats Rock! You receive 1 point';
         }
         else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-            userCounter++
+            incrementUsercount()
             console.log("Computer score is " + computerCounter);
             console.log("User Score is " + userCounter)
             return 'You Win! Scissors beats Paper! You receive 1 point';
@@ -57,10 +57,17 @@ function playRound (playerSelection, computerSelection) {
             console.log("User Score is " + userCounter)
             return "It's a tie! No points"
         }
-
         
 }
 
+// create function to increment and total up the score count between user and computer
+function incrementComputerCount() {
+    computerCounter++;
+}
+
+function incrementUsercount() {
+    userCounter++;
+}
 
 const playerSelection = prompt("Pick Rock, Paper, or Scissors")
 console.log("You selected " + playerSelection.toLowerCase())
@@ -68,11 +75,24 @@ const computerSelection = getComputerChoice(options);
 console.log(playRound(playerSelection, computerSelection))
 
 function game() {
-    playRound(playerSelection, computerSelection)
-    playRound(playerSelection, computerSelection)
-    playRound(playerSelection, computerSelection)
-    playRound(playerSelection, computerSelection)
-    playRound(playerSelection, computerSelection)
+    
+    // define the playerSelection and ComputerSelection variables again to ensure they're in
+    // function scope of game()
+    // Loop through the playRound() function 5 times
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Pick Rock, Paper, or Scissors");
+        const computerSelection = getComputerChoice(options);
+        console.log("You selected " + playerSelection.toLowerCase());
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+    if (computerCounter > userCounter) {
+        console.log("Computer won with a final score of " + computerCounter);
+    }
+        else {
+            console.log("User won with a final score of " + userCounter);
+    }   
+    
 }
 
 game();
