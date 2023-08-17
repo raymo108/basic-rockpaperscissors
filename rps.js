7f2ae660f3b8cd1d50058df4e0caf403ec202f2c
Shane Raymond <60206015+raymo108@users.noreply.github.com>
2023-08-17 17:13:28 -0400

    Refactor rps to include buttons and DOM manip

--- rps.js
+++ rps.js
@@ -1,98 +1,107 @@
 // create an array to store the random choice from computerChoice
 const options = ['rock', 'paper', 'scissors']
 
-function getComputerChoice(options) {      // Create a function called getComputerChoice
-    const computerChoice = Math.floor(Math.random() * options.length); // Pick a random index in the array of options
-    return options[computerChoice]; // return the element within the generated index
-
+function getComputerChoice() {      // Create a function called getComputerChoice
+    let randomIndex = Math.floor(Math.random() * options.length);
+    let computerSelection = options[randomIndex];
+    return computerSelection;
 }
 
-let computerCounter = 0;
 let userCounter = 0;
+let computerCounter = 0;
 
-function playRound (playerSelection, computerSelection) {
-    // convert playerSelection to lowercase no matter what for case-sensitivty
-    playerSelection = playerSelection.toLowerCase();
-    console.log("Computer selected " + computerSelection);
-  
- 
+function addtoUserCounter() {
+    userCounter++
+}
+function addtoComputerCounter () {
+    computerCounter++
+}
+
+const container = document.querySelector('#results');
+
+function playRound(playerSelection,computerSelection) {
+    const content = document.createElement('div');
+    content.classList.add('content');
     if (playerSelection === 'rock' && computerSelection === 'paper') {
-        incrementComputerCount();
-        console.log("Computer score is " + computerCounter);
-        console.log("User Score is " + userCounter)
-        return 'You Lose! Paper beats Rock! 1 point for Computer';
+        addtoComputerCounter()
+        content.textContent =  'You chose rock and computer chose paper. You lose! Paper beats rock. Computer Score: ' + computerCounter  + ' User Score: ' + userCounter;    
     }
         else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            incrementComputerCount()
-            console.log("Computer score is " + computerCounter);
-            console.log("User Score is " + userCounter)
-            return 'You Lose! Scissors beats Paper! 1 point for Computer';
+            addtoComputerCounter()
+            content.textContent = 'You chose paper and computer chose scissors. You lose! scissors beats paper. Computer Score: ' + computerCounter + ' User score: ' + userCounter;
         }
         else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            incrementComputerCount()
-            console.log("Computer score is " + computerCounter);
-            console.log("User Score is " + userCounter)
-            return 'You Lose! Rock beats Scissors! 1 point for Computer';
+            addtoComputerCounter()
+            content.textContent =  'You chose scissors and computer chose rock. You lose! rock beats scissors. Computer Score: ' + computerCounter + ' User score: ' + userCounter;
         }
         else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            incrementUsercount()
-            console.log("Computer score is " + computerCounter);
-            console.log("User Score is " + userCounter)
-            return 'You Win! Rock beats Scissors! You receive 1 point';
+            addtoUserCounter()
+            content.textContent =  'You chose rock and computer chose scissors. You win! rock beats scissors. Computer score: ' + computerCounter + ' User score: ' + userCounter;
         }
-        else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            incrementUsercount()
-            console.log("Computer score is " + computerCounter);
-            console.log("User Score is " + userCounter)
-            return 'You Win! Paper beats Rock! You receive 1 point';
+        else if (playerSelection ==='paper' && computerSelection ==='rock') {
+            addtoUserCounter()
+            content.textContent =  'You chose paper and computer chose rock. You win! paper beats rock. Computer score: ' + computerCounter + ' User Score: ' + userCounter;
         }
-        else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-            incrementUsercount()
-            console.log("Computer score is " + computerCounter);
-            console.log("User Score is " + userCounter)
-            return 'You Win! Scissors beats Paper! You receive 1 point';
+        else if (playerSelection ==='scissors' && computerSelection === 'paper') {
+            addtoUserCounter()
+            content.textContent =  'You chose scissors and computer chose paper. You win! scissors beats paper. Computer Score: ' + computerCounter + ' User score: ' + userCounter;
         }
         else if (playerSelection === computerSelection) {
-            console.log("Computer score is " + computerCounter);
-            console.log("User Score is " + userCounter)
-            return "It's a tie! No points"
+            content.textContent = "It's a tie! No points awarded. Computer score: " + computerCounter + ' User score: ' + userCounter;
         }
-        
+        results.appendChild(content);
 }
 
-// create function to increment and total up the score count between user and computer
-function incrementComputerCount() {
-    computerCounter++;
-}
+const button = document.createElement('div');
+button.style.backgroundColor = 'yellow'
+button.style.fontSize = '32px'
 
-function incrementUsercount() {
-    userCounter++;
-}
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
 
-const playerSelection = prompt("Pick Rock, Paper, or Scissors")
-console.log("You selected " + playerSelection.toLowerCase())
-const computerSelection = getComputerChoice(options);
-console.log(playRound(playerSelection, computerSelection))
-
-function game() {
-    
-    // define the playerSelection and ComputerSelection variables again to ensure they're in
-    // function scope of game()
-    // Loop through the playRound() function 5 times
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Pick Rock, Paper, or Scissors");
-        const computerSelection = getComputerChoice(options);
-        console.log("You selected " + playerSelection.toLowerCase());
-        console.log(playRound(playerSelection, computerSelection));
+rockButton.addEventListener('click', () => {
+    if (userCounter === 5) {
+        button.textContent = 'User Wins with a final score of ' + userCounter; 
     }
-
-    if (computerCounter > userCounter) {
-        console.log("Computer won with a final score of " + computerCounter);
-    }
+        else if (computerCounter === 5) {
+            button.textContent = 'Computer wins with a final score of ' + computerCounter;
+        }
         else {
-            console.log("User won with a final score of " + userCounter);
-    }   
-    
-}
+            playerSelection = 'rock';
+            computerSelection = getComputerChoice();
+            playRound(playerSelection, computerSelection);
+        }
+});
 
-game();
+paperButton.addEventListener('click', () => {
+    if (userCounter === 5) {
+        button.textContent = 'User Wins with a final score of ' + userCounter; 
+    }
+        else if (computerCounter === 5) {
+            button.textContent = 'Computer wins with a final score of ' + computerCounter;
+        }
+        else {
+            playerSelection = 'paper';
+            computerSelection = getComputerChoice();
+            playRound(playerSelection, computerSelection);
+        }
+});
+
+scissorsButton.addEventListener('click', () => {
+    if (userCounter === 5) {
+        button.textContent = 'User Wins with a final score of ' + userCounter; 
+    }
+        else if (computerCounter === 5) {
+            button.textContent = 'Computer wins with a final score of ' + computerCounter;
+        }
+        else {
+            playerSelection = 'scissors';
+            computerSelection = getComputerChoice();
+            playRound(playerSelection, computerSelection);
+        }
+    
+});
+
+results.appendChild(button);
+
